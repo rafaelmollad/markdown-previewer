12714a678fb4d79ab25db27fb4707b8da3dc9592
Rafael Molla <rafaelmolladouglas@gmail.com>
2018-12-26 09:42:30 -0300

    Updates

---
+++ precache-manifest.6bb22b143a130a05346f81b883407818.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2c275135a06121482d1",
+    "url": "/markdown-previewer/static/js/runtime~main.b2c27513.js"
+  },
+  {
+    "revision": "55395b2ed3d93edfcdad",
+    "url": "/markdown-previewer/static/js/main.55395b2e.chunk.js"
+  },
+  {
+    "revision": "d04808ab351b03c9f95d",
+    "url": "/markdown-previewer/static/js/1.d04808ab.chunk.js"
+  },
+  {
+    "revision": "55395b2ed3d93edfcdad",
+    "url": "/markdown-previewer/static/css/main.ee7afa7a.chunk.css"
+  },
+  {
+    "revision": "e713bf5048292a9fd16809a624f5c852",
+    "url": "/markdown-previewer/index.html"
+  }
+];
